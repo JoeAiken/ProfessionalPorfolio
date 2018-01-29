f66e6aef7034e357f3aea09c90650ceb7bac739b
Joseph Aiken <josephaiken92@gmail.com>
2018-01-29 14:00:25 +0000

    reset file strucuture, added nodemailer

---
+++ routes/emailRoute.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+var Transport = nodemailer.createTransport({
+            service: "gmail",
+            host: process.env.EMAIL,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            });
+
+        module.exports = function(app, flag) {
+
+            app.get('/', function(req, res) {
+                res.sendfile('index.html');
+            });
+            app.get('/send', function(req, res) {
+                var mailOptions = {
+                    to: req.query.to,
+                    subject: req.query.subject,
+                    html: req.query.html
+                };
+                console.log(mailOptions);
+                Transport.sendMail(mailOptions, function(error, response) {
+                    if (error) {
+                        console.log(error);
+                        res.end("error");
+                    }
+                    else {
+                        console.log("Message sent: " + response.message);
+                        res.end("sent");
+                    }
+                });
+            });
+        };
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+//------static directory--------------
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//----------routes-----------------
+require("./routes/emailRoute.js")(app);
+
+
+//----------port check---------------
+app.listen(port, function(req, res) {
+    console.log("Server is running on port: ", port);
+});
