1dd8d1b16e070d493e5975a2948fa6b72289513a
Joseph Aiken <josephaiken92@gmail.com>
2018-01-29 15:24:35 +0000

    changed emailRoutes

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -1,34 +1,36 @@
 const nodemailer = require('nodemailer');
 
 var Transport = nodemailer.createTransport({
-            service: "gmail",
-            host: process.env.EMAIL,
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
-            });
+    service: "gmail",
+    host: EMAIL,
+    auth: {
+        user: EMAIL,
+        pass: PASSWORD
 
-        module.exports = function(app, flag) {
+    }
+});
 
-            app.get('/', function(req, res) {
-                res.sendfile('index.html');
-            });
-            app.get('/send', function(req, res) {
-                var mailOptions = {
-                    to: req.query.to,
-                    subject: req.query.subject,
-                    html: req.query.html
-                };
-                console.log(mailOptions);
-                Transport.sendMail(mailOptions, function(error, response) {
-                    if (error) {
-                        console.log(error);
-                        res.end("error");
-                    }
-                    else {
-                        console.log("Message sent: " + response.message);
-                        res.end("sent");
-                    }
-                });
-            });
+module.exports = function(app, flag) {
+
+    app.get('/', function(req, res) {
+        res.sendfile('index.html');
+    });
+    app.get('/send', function(req, res) {
+        var mailOptions = {
+            to: req.query.to,
+            subject: req.query.subject,
+            html: req.query.html
         };
+        console.log(mailOptions);
+        Transport.sendMail(mailOptions, function(error, response) {
+            if (error) {
+                console.log(error);
+                res.end("error");
+            }
+            else {
+                console.log("Message sent: " + response.message);
+                res.end("sent");
+            }
+        });
+    });
+};
